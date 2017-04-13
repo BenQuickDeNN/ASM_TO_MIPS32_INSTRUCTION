a0f2aa964d38c0db9bb860ff3251a811ea67b0a1
BenQuickDeNN <2413269761@qq.com>
2017-04-13 22:38:21 +0800

    first commit

---
+++ ASM2BIN/HashOPcode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ASM2BIN
+{
+    /// <summary>
+    /// 操作码哈希表
+    /// </summary>
+    class HashOPcode
+    {
+    }
+}
---
+++ ASM2BIN/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ASM2BIN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String fileSource = args[0];// 源文件地址
+            String fileDest = args[1];// 目标文件地址
+            if (!File.Exists(fileSource))
+            {
+                Console.WriteLine("找不到源文件!");
+                return;
+            }
+            
+        }
+    }
+}
